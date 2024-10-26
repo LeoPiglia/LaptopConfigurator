b0155cac8a554f3eab7acf753a53c0d73bacbf47
leonardo <leonardo.pigliacampo02@gmail.com>
2024-10-26 16:41:57 +0200

    Refactored class Usb and Hdmi

--- app/src/main/java/org/example/configurator/Hdmi.java
+++ app/src/main/java/org/example/configurator/Hdmi.java
@@ -1,12 +1,63 @@
 package org.example.configurator;
 
-public class Hdmi {
+/**
+ * Represents the Hdmi object of a laptop.
+ */
+public class Hdmi extends Port{
 
-    // Aggiungere attributi corrispondenti alle proprietà dell'ontologia
+    private String hdmiVersion;
 
-    public Hdmi() {
-        // Costruttore di default
+    /**
+     * Constructs a Hdmi object with the specified laptop and Hdmi version
+     * @param laptopOfPort the laptop to which the Hdmi object is associated
+     * @param hdmiVersion the Hdmi version of the Hdmi object
+     */
+
+    public Hdmi(Laptop laptopOfPort, String hdmiVersion) {
+        super(laptopOfPort);
+        this.hdmiVersion = hdmiVersion;
     }
 
-    // Aggiungere metodi getter e setter
+    /**
+     * Gets the version of the HDMI port.
+     *
+     * @return the HDMI version
+     */
+    public String getHdmiVersion() {
+        return hdmiVersion;
+    }
+
+    /**
+     * Sets the version of the HDMI port.
+     *
+     * @param hdmiVersion the HDMI version (e.g., 1.4, 2.0, 2.1)
+     */
+    public void setHdmiVersion(String hdmiVersion) {
+        this.hdmiVersion = hdmiVersion;
+    }
+
+    /**
+     * Clones the Hdmi object
+     * @param laptop the laptop to which the Hdmi object is associated
+     * @return a new Hdmi object
+     */
+    /**
+     * Clones the HDMI port with a different laptop.
+     *
+     * @param laptop the laptop to which the HDMI port will be associated
+     * @return a new Hdmi object with the same HDMI version but a new laptop association
+     */
+    public Hdmi clone(Laptop laptop) {
+        return new Hdmi(laptop, this.hdmiVersion);
+    }
+
+    /**
+     *
+     * @return the laptop to which the Hdmi object is associated
+     */
+    public String toString() {
+        return "Hdmi{" +
+                "hdmiVersion='" + hdmiVersion + '\'' +
+                '}';
+    }
 }
--- app/src/main/java/org/example/configurator/Usb.java
+++ app/src/main/java/org/example/configurator/Usb.java
@@ -1,12 +1,61 @@
 package org.example.configurator;
 
-public class Usb {
+/**
+ * Represents the Usb object of a laptop.
+ */
+public class Usb extends Port {
 
-    // Aggiungere attributi corrispondenti alle proprietà dell'ontologia
+    private String usbType;
 
-    public Usb() {
-        // Costruttore di default
+    /**
+     * Constructs a Usb object with the specified laptop and USB type.
+     *
+     * @param laptopOfUsb the laptop to which the Usb object is associated
+     * @param usbType the type of the USB (e.g., USB-A, USB-C, USB 3.0)
+     */
+    public Usb(Laptop laptopOfUsb, String usbType) {
+        super(laptopOfUsb);
+        this.usbType = usbType;
     }
 
-    // Aggiungere metodi getter e setter
+    /**
+     * Gets the type of the USB port.
+     *
+     * @return the USB type
+     */
+    public String getUsbType() {
+        return usbType;
+    }
+
+    /**
+     * Sets the type of the USB port.
+     *
+     * @param usbType the USB type (e.g., USB-A, USB-C, USB 3.0)
+     */
+    public void setUsbType(String usbType) {
+        this.usbType = usbType;
+    }
+
+    /**
+     * Clones the USB port with a different laptop.
+     *
+     * @param laptop the laptop to which the USB port will be associated
+     * @return a new Usb object with the same USB type but a new laptop association
+     */
+
+    public Usb clone(Laptop laptop) {
+        return new Usb(laptop, this.usbType);
+    }
+
+    /**
+     * Returns a string representation of the USB port.
+     *
+     * @return a string representation of the USB port
+     */
+    @Override
+    public String toString() {
+        return "Usb{" +
+                "usbType='" + usbType + '\'' +
+                '}';
+    }
 }
