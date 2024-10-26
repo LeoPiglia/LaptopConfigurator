dc7a1d755d33db7685e22ba951fb61dbf3714dde
leonardo <leonardo.pigliacampo02@gmail.com>
2024-10-26 17:23:09 +0200

    Refactored class CoolingSystem

--- app/src/main/java/org/example/configurator/CoolingSystem.java
+++ app/src/main/java/org/example/configurator/CoolingSystem.java
@@ -2,11 +2,96 @@ package org.example.configurator;
 
 public class CoolingSystem {
 
-    // Aggiungere attributi corrispondenti alle proprietà dell'ontologia
+    private Laptop laptopOfCoolingSystem;
+    private String coolingSystemName;
+    private String coolingSystemType;
 
-    public CoolingSystem() {
-        // Costruttore di default
+    /**
+     * Constructs a CoolingSystem with the specified laptop, name and type
+     *
+     * @param laptopOfCoolingSystem the laptop to which the cooling system is associated
+     * @param coolingSystemName     the name of the cooling system
+     * @param coolingSystemType     the type of the cooling system
+     */
+
+    public CoolingSystem(Laptop laptopOfCoolingSystem, String coolingSystemName, String coolingSystemType) {
+        this.laptopOfCoolingSystem = laptopOfCoolingSystem;
+        this.coolingSystemName = coolingSystemName;
+        this.coolingSystemType = coolingSystemType;
     }
 
-    // Aggiungere metodi getter e setter
+    /**
+     * Clones the CoolingSystem object
+     *
+     * @param laptop the laptop to which the CoolingSystem object is associated
+     * @return a new CoolingSystem object
+     */
+
+    public CoolingSystem clone(Laptop laptop) {
+        return new CoolingSystem(laptop, this.coolingSystemName, this.coolingSystemType);
+    }
+
+    /**
+     * @return the laptop to which the cooling system is associated
+     */
+
+    public Laptop getLaptopOfCoolingSystem() {
+        return laptopOfCoolingSystem;
+    }
+
+    /**
+     * sets the laptop to which the cooling system is associated
+     *
+     * @param laptopOfCoolingSystem the laptop to which the cooling system is associated
+     */
+
+    public void setLaptopOfCoolingSystem(Laptop laptopOfCoolingSystem) {
+        this.laptopOfCoolingSystem = laptopOfCoolingSystem;
+    }
+
+    /**
+     * @return the name of the cooling system
+     */
+
+    public String getCoolingSystemName() {
+        return coolingSystemName;
+    }
+
+    /**
+     * sets the name of the cooling system
+     * @param coolingSystemName the name of the cooling system
+     */
+
+    public void setCoolingSystemName(String coolingSystemName) {
+        this.coolingSystemName = coolingSystemName;
+    }
+
+    /**
+     * @return the type of the cooling system
+     */
+
+    public String getCoolingSystemType() {
+        return coolingSystemType;
+    }
+
+    /**
+     * sets the type of the cooling system
+     * @param coolingSystemType the type of the cooling system
+     */
+
+    public void setCoolingSystemType(String coolingSystemType) {
+        this.coolingSystemType = coolingSystemType;
+    }
+
+    /**
+     * @return the laptop to which the cooling system is associated
+     */
+
+    public String toString() {
+        return "CoolingSystem{" +
+                "laptopOfCoolingSystem=" + laptopOfCoolingSystem +
+                ", coolingSystemName='" + coolingSystemName + '\'' +
+                ", coolingSystemType='" + coolingSystemType + '\'' +
+                '}';
+    }
 }
