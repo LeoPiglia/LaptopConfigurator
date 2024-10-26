b27b3ea37a59feddc4ddc31365d55c3d67f9d631
leonardo <leonardo.pigliacampo02@gmail.com>
2024-10-26 17:17:59 +0200

    Refactored class AudioSystem

--- app/src/main/java/org/example/configurator/AudioSystem.java
+++ app/src/main/java/org/example/configurator/AudioSystem.java
@@ -1,12 +1,98 @@
 package org.example.configurator;
 
+/**
+ * This class represents the audio system of a laptop
+ */
 public class AudioSystem {
 
-    // Aggiungere attributi corrispondenti alle proprietà dell'ontologia
+    private Laptop laptopOfAudioSystem;
+    private String audioSystemName;
+    private String audioSystemPower;
 
-    public AudioSystem() {
-        // Costruttore di default
+    /**
+     * Constructs an AudioSystem with the specified laptop and power
+     * @param laptopOfAudioSystem the laptop to which the audio system is associated
+     * @paam audioSystemName the name of the audio system
+     * @param audioSystemPower the power of the audio system
+     */
+    public AudioSystem(Laptop laptopOfAudioSystem, String audioSystemPower, String audioSystemName) {
+        this.laptopOfAudioSystem = laptopOfAudioSystem;
+        this.audioSystemPower = audioSystemPower;
+        this.audioSystemName = audioSystemName;
+
     }
 
-    // Aggiungere metodi getter e setter
+    /**
+     * Clones the AudioSystem object
+     * @param laptop the laptop to which the AudioSystem object is associated
+     * @return a new AudioSystem object
+     */
+    public AudioSystem clone(Laptop laptop) {
+        return new AudioSystem(laptop, this.audioSystemPower, this.audioSystemName);
+    }
+
+    /**
+     *
+     * @return the laptop to which the audio system is associated
+     */
+    public Laptop getLaptopOfAudioSystem() {
+        return laptopOfAudioSystem;
+    }
+
+    /**
+     * sets the laptop to which the audio system is associated
+     * @param laptopOfAudioSystem the laptop to which the audio system is associated
+     */
+    public void setLaptopOfAudioSystem(Laptop laptopOfAudioSystem) {
+        this.laptopOfAudioSystem = laptopOfAudioSystem;
+    }
+
+    /**
+     *
+     * @return the power of the audio system
+     */
+    public String getAudioSystemPower() {
+        return audioSystemPower;
+    }
+
+    /**
+     * sets the power of the audio system
+     * @param audioSystemPower the power of the audio system
+     */
+    public void setAudioSystemPower(String audioSystemPower) {
+        this.audioSystemPower = audioSystemPower;
+    }
+
+    /**
+     *
+     * @return the name of the audio system
+     */
+
+    public String getAudioSystemName() {
+        return audioSystemName;
+
+    }
+
+    /**
+     * sets the name of the audio system
+     * @param audioSystemName the name of the audio system
+     */
+
+    public void setAudioSystemName(String audioSystemName) {
+        this.audioSystemName = audioSystemName;
+    }
+
+    /**
+     *
+     * @return a string representation of the AudioSystem object
+     */
+    @Override
+    public String toString() {
+        return "AudioSystem{" +
+                "laptopOfAudioSystem=" + laptopOfAudioSystem +
+                ", audioSystemName='" + audioSystemName + '\'' +
+                ", audioSystemPower='" + audioSystemPower + '\'' +
+                '}';
+    }
+
 }
