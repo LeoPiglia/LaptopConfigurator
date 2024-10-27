3b0767e206bd2297fae699ab862e20806f0cc00b
leonardo <leonardo.pigliacampo02@gmail.com>
2024-10-27 12:10:19 +0100

    Refactored class colour

--- app/src/main/java/org/example/configurator/Colour.java
+++ app/src/main/java/org/example/configurator/Colour.java
@@ -1,12 +1,74 @@
 package org.example.configurator;
 
+/**
+ * This class represents the colour of a laptop
+ */
 public class Colour {
 
-    // Aggiungere attributi corrispondenti alle proprietà dell'ontologia
+    private String colourName;
 
-    public Colour() {
-        // Costruttore di default
+    private Laptop colourOfLaptop;
+
+    /**
+     * Constructs a Colour with the specified laptop and name
+     * @param colourOfLaptop the laptop to which the colour is associated
+     * @param colourName the name of the colour
+     */
+
+    public Colour(Laptop colourOfLaptop, String colourName) {
+        this.colourOfLaptop = colourOfLaptop;
+        this.colourName = colourName;
     }
 
-    // Aggiungere metodi getter e setter
+    /**
+     * Clones the Colour object
+     * @param laptop the laptop to which the Colour object is associated
+     * @return a new Colour object
+     */
+
+    public Colour clone(Laptop laptop) {
+        return new Colour(laptop, this.colourName);
+    }
+
+    /**
+     * @return the laptop to which the colour is associated
+     */
+
+    public Laptop getColourOfLaptop() {
+        return colourOfLaptop;
+    }
+
+    /**
+     * sets the laptop to which the colour is associated
+     * @param colourOfLaptop the laptop to which the colour is associated
+     */
+
+    public void setColourOfLaptop(Laptop colourOfLaptop) {
+        this.colourOfLaptop = colourOfLaptop;
+    }
+
+    /**
+     * @return the name of the colour
+     */
+
+    public String getColourName() {
+        return colourName;
+    }
+
+    /**
+     * sets the name of the colour
+     * @param HasColourName the name of the colour
+     */
+
+    public void setColourName(String HasColourName) {
+        this.colourName = HasColourName;
+    }
+
+    /**
+     * @return the name of the colour
+     */
+    @Override
+    public String toString() {
+        return "Colour: " + colourName;
+    }
 }
